a4d3651d158934b68f3c731acbfaeac4725ab9e9
Kirill <yakubov.kirill.work@gmail.com>
2020-07-11 21:54:14 +0300

    add cities to database

--- countries/city_manager/views.py
+++ countries/city_manager/views.py
@@ -8,9 +8,19 @@ def index(request):
     countries = Country.objects.all()
     return render(request, "city_manager/index.html", {"countries": countries})
 
+# Add country to database
 def addCount(request):
     if request.method == "POST":
         count = Country()
         count.country_name = request.POST.get("country")
         count.save()
+    return HttpResponseRedirect("/")
+
+# Add city to database
+def addCity(request):
+    if request.method == "POST":
+        city = City()
+        city.city_name = request.POST.get("city")
+        city.country = request.POST.get("country_select")
+        city.save()
     return HttpResponseRedirect("/")
--- countries/countries/urls.py
+++ countries/countries/urls.py
@@ -5,5 +5,6 @@ from city_manager import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('create/', views.addCount),
+    path('create-city/', views.addCity),
     path('', views.index)
 ]
